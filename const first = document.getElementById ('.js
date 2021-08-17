d01983a5216a2a1642a7b7b16c158df34d77e372
taiorbach <88382699+taiorbach@users.noreply.github.com>
2021-08-17 13:18:00 +0300

    Delete const first = document.getElementById ('.js

--- const first = document.getElementById ('.js
+++
@@ -1,6 +0,0 @@
-const first = document.getElementById ('first')
-const second = document.getElementById ('second')
-const third = document.getElementById ('third')
-const firstValue = first.Value
-const secondValue = second.Value
-const thirdValue = third.Value
