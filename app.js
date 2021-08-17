c6a43428d7f833036109eab21702284d438c6224
taiorbach <88382699+taiorbach@users.noreply.github.com>
2021-08-17 13:16:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function calculate() {
+    let first = document.getElementById ('first')
+    let second = document.getElementById ('second')
+     document.getElementById ('third').value = first.value + second.value
+}
